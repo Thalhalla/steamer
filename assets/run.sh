a77a38dabb32ec4c0e20337cf8e8023990cc0232
Josh Cox <josh@webhosting.coop>
2017-01-01 13:47:47 -0600

    bash

--- assets/run.sh
+++ assets/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SHELL=/bin/bash
 # cd /home/steam/SteamLibrary
 #cd '/home/steam/Steam/steamapps/common/Counter-Strike Global Offensive Beta - Dedicated Server'
 cd /home/steam/serverfiles/csgo_ds
@@ -6,4 +7,4 @@ cd /home/steam/serverfiles/csgo_ds
 echo "GLST doc: https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive_Dedicated_Servers#Registering_Game_Server_Login_Token"
 # ./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 1 +mapgroup mg_demolition +map de_lake +sv_setsteamaccount $STEAM_GLST  +ip $IP
 # ./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 1 +mapgroup mg_demolition +map de_lake +sv_setsteamaccount $STEAM_GLST  -port $PORT
-./srcds_run -game csgo -console -usercon +game_type $CS_GAME_TYPE +game_mode $CS_GAME_MODE +mapgroup $CS_MAP_GROUP +map $CS_INITIAL_MAP +sv_setsteamaccount $STEAM_GLST  -port $PORT
+/bin/bash ./srcds_run -game csgo -console -usercon +game_type $CS_GAME_TYPE +game_mode $CS_GAME_MODE +mapgroup $CS_MAP_GROUP +map $CS_INITIAL_MAP +sv_setsteamaccount $STEAM_GLST  -port $PORT
