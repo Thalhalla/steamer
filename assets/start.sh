26414bc2801f18032d1e299ae48909c7a222619c
Josh Cox <josh@webhosting.coop>
2017-01-04 11:00:40 -0600

    untarsteamcmd

--- assets/start.sh
+++ assets/start.sh
@@ -5,6 +5,7 @@ mkdir -p /home/steam/serverfiles/csgo_ds
 
 cp /assets/steamer.txt /home/steam/
 cd /home/steam
+tar zxf /opt/steamer/steamcmd_linux.tar.gz
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
