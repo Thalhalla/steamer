03ff5a6e9816af6148d86c130db1f4443ee5de51
Josh Cox <josh@webhosting.coop>
2017-01-04 09:39:23 -0600

    sh

--- assets/start.sh
+++ assets/start.sh
@@ -8,5 +8,5 @@ cd /home/steam
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
-steamcmd +runscript /home/steam/steamer.txt
+./steamcmd.sh +runscript /home/steam/steamer.txt
 bash /assets/run.sh
