6bc87ca15d9b206b1515fcdb5069946dc2b68cfd
Josh Cox <josh@webhosting.coop>
2017-01-04 21:32:57 -0600

    opt/steam is now the home for steamcmd.sh

--- assets/start.sh
+++ assets/start.sh
@@ -5,9 +5,11 @@ mkdir -p /home/steam/serverfiles/csgo_ds
 
 cp /assets/steamer.txt /home/steam/
 cd /home/steam
-tar zxf /opt/steamer/steamcmd_linux.tar.gz
 sed -i "s/REPLACEME_USERNAME/$STEAM_USERNAME/" steamer.txt
 sed -i "s/REPLACEME_PASSWORD/$STEAM_PASSWORD/" steamer.txt
 sed -i "s/REPLACEME_GID/$STEAM_GID/" steamer.txt
+
+cd /opt/steam
+tar zxf /opt/steamer/steamcmd_linux.tar.gz
 ./steamcmd.sh +runscript /home/steam/steamer.txt
 bash /assets/run.sh
